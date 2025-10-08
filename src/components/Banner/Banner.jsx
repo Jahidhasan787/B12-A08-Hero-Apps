19f89c7d16f7bce566ee8a2d294f3ee8215fb537
Jahid <jahidict56@gmail.com>
2025-10-08 13:58:48 +0600

    added banner section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import heroImage from "../../assets/hero.png";
+import googleStore from "../../assets/images.png";
+import appStore from "../../assets/icon_appstore__ev0z770zyxoy_large_2x.png";
+
+const Banner = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className=" text-5xl font-extrabold">
+        We Build <br />
+        <span className="text-[#632EE3]">Productive</span> Apps
+      </h1>
+      <p className="py-5 text-[#627382]">
+        At HERO.IO, we craft innovative apps designed to make everyday life
+        simpler, smarter, and more exciting. <br />
+        Our goal is to turn your ideas into digital experiences that truly make
+        an impact.
+      </p>
+      <div className="flex justify-center pb-5 gap-5">
+        <button className="btn p-5">
+          <img src={googleStore} className="w-[30px]" alt="" />
+          Google Play
+        </button>
+        <button className="btn p-5">
+          {" "}
+          <img src={appStore} className="w-[25px]" alt="" />
+          App Store
+        </button>
+      </div>
+      <img className="w-[50%] mx-auto" src={heroImage} alt="" />
+      <div className="text-white bg-gradient-to-r from-[#632EE3] to-[#9F62F2] py-20">
+        <h2 className="text-4xl font-bold pb-10">Trusted by Millions, Built for You</h2>
+        <div className="flex justify-between items-center w-[50%] mx-auto">
+          <ul>
+            <li>Total Downloads</li>
+            <li className="text-5xl font-bold py-3">29.6M</li>
+            <li>21% more than last month</li>
+          </ul>
+          <ul>
+            <li>Total Reviews</li>
+            <li className="text-5xl font-bold py-3">906K</li>
+            <li>46% more than last month</li>
+          </ul>
+          <ul>
+            <li>Active Apps</li>
+            <li className="text-5xl font-bold py-3">132+</li>
+            <li>31 more will Launch</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     </>
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-100 shadow-sm md:px-[40px]">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
